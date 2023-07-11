aee2e32c5e2fdb3416868286e347556ba834b9e2
jiahao-yang <jiahao_yang@qq.com>
2023-07-11 21:15:55 +0800

    add .gitignore

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-brew install portaudio
-brew install flac
-brew install ffmpeg
+python -m venv venv
+. venv/bin/activate
+
+# MacOS only
+# brew install portaudio
+# brew install flac
+# brew install ffmpeg
+
+# Linux only
+sudo apt-get install portaudio19-dev
+sudo apt-get install flac
+sudo apt-get install ffmpeg
 
 pip install -r ./requirements.txt
